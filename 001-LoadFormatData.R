a75712fb93a123db27ec5052b5cdc57c000f986f
mgarce3 <mgarce3@lsu.edu>
2026-02-23 11:10:01 -0600

    got counterbalance column

--- 001-LoadFormatData.R
+++ 001-LoadFormatData.R
@@ -2,15 +2,33 @@ library(dplyr)
 library(tidyr)
 library(purrr)
 library(stringr)
+library(readxl)
 library(ggplot2)
-#Laptop
+
+# Set up datapath
+##Laptop
 datapath <- list.files(path = "C:/Users/westa/OneDrive - Louisiana State University/WordAssociationRTWM/data_WMRT", full.names = TRUE)
-#Work
+##Work
 #datapath <- list.files(path = "C:/Users/Stan/OneDrive - Louisiana State University/WordAssociationRTWM/data_WMRT", full.names = TRUE)
+##Meghan
+unzip("data_WMRT.zip")
+datapath <- list.files("data_WMRT", full.names = TRUE)
+
 datapath <- datapath[-73]
 
+# Read in cue
 words_meta <- read.csv("data/stim_64_NNVB.csv")
 
+# Get participant counterbalances
+temp <- read_xlsx("TTAWM Counterbalance tracker.xlsx")[c(3,6)]
+pptracker <- temp %>% 
+  na.omit(temp) %>% 
+  mutate(participant = str_remove(PPID, "TTA_"), .before = Counterbalance) %>% 
+  mutate(participant = str_remove(participant, "^0+")) %>% 
+  rename(counterbalance = Counterbalance) %>% 
+  select(-PPID)
+
+# Make metadata df
 x <- map_dfr(datapath, function(x){
     exp_load <- read.csv(list.files(x,pattern = "TTA_[0-9][0-9][0-9]_LOAD_[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]trialstest.csv",full.names = TRUE))[1:32,] 
     exp_noload <- read.csv(list.files(x,pattern = "TTA_[0-9][0-9][0-9]_NO_LOAD_[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]trialstest.csv",full.names = TRUE))[1:32,] 
@@ -45,12 +63,23 @@ x <- map_dfr(datapath, function(x){
              square_rt_mili = square_rt * 1000,
              cue_rt_mili = cue_rt * 1000,
              type_dur_mili = type_dur * 1000) %>%
-      left_join(select(words_meta,cue,strength_strat,type), by = "cue")
+      left_join(select(words_meta,cue,strength_strat,type), by = "cue") %>% 
+      group_by(participant) %>% 
+      mutate(counterbalance = ifelse((participant %in% pptracker$participant), pptracker$Counterbalance[pptracker$participant %in% participant], 0), .after = sq_resp) %>% 
+      ungroup()
     return(d_all)
 })
 
+# What I used to make sure the counterbalances all lined up
+test <- x %>% 
+  select(-cue) %>% 
+  select(participant, counterbalance) %>% 
+  unique()
 
+## any rows in test that don't match pptracker (excluded 73-78, 82)
+verify <- anti_join(pptracker, test, by = "participant", "counterbalance")
 
+# combine psychling data to x like it is done in 001 script combined_meta with left join
 
-
+# Save out metadata df
 saveRDS(x, paste0("data/TTA_metadata_",Sys.Date(),".rds"))
