f355d97d98a9ee0c5c318077dc7ba48c60cf0ddf
mgarce3 <mgarce3@lsu.edu>
2026-03-02 11:17:28 -0600

    merge

---
+++ 004-ResponseTimeAnalysis.R
@@ -0,0 +1,131 @@
+library(ggplot2)
+library(dplyr)
+library(readr)
+library(tidyr)
+library(lme4)
+library(rstatix)
+library(lmerTest)
+library(emmeans)
+source("R/Load_Helpers.R")
+
+z <- function(x){
+  return((x - mean(x,na.rm = T))/sd(x,na.rm = T))
+}
+
+
+## Load data 
+d <- load_most_recent_by_mtime("data/", pattern = "TTA2_meta_response_filtered-")
+
+
+############ Response time to Word Association ################################
+
+ggplot(d, aes(x = z_cue_rt_mili,fill =context))+
+  geom_histogram()+
+  facet_grid(condition~context)
+
+ggplot(d, aes(x = condition, y = cue_rt_mili))+
+  stat_summary(fun = "mean", geom="col")
+
+## Looking at counterbalances: looks like people are faster in block 2 
+## regardless of condition - maybe practice effects/wanting to be done. 
+ggplot(d, aes(x = context, y = cue_rt_mili, fill = counterbalance))+
+  stat_summary(fun = "mean", geom = "col", position = "dodge")+
+  facet_grid(block~condition, labeller = "label_both") +
+  geom_text(stat = "summary",fun = "mean",vjust = 12, aes(label = round(after_stat(y),2)),
+            position = position_dodge(0.9))+
+  theme_bw()
+
+
+## Difference plots
+d_diff <- d %>%
+  group_by(context,condition,block,counterbalance) %>%
+  summarize(m = mean(cue_rt_mili)) %>%
+  group_by(context,block) %>%
+  mutate(diff_cond = m[condition == "load"] - m[condition == "no_load"]) %>%
+  group_by(context,condition) %>%
+  mutate(diff_block = m[block == 2] - m[block == 1]) %>%
+  ungroup()
+
+ggplot(d_diff, aes(x = context, y = diff_cond, fill = block))+
+  stat_summary(fun = "identity", geom = "col",position = "dodge")+
+  theme_bw()+
+  annotate("text",label = "FASTER IN LOAD",x=2.25,y = 125,size = 8)+
+  annotate("text",label = "FASTER IN NO LOAD",x=2.25,y = -125,size = 8)
+
+
+
+ggplot(d_diff, aes(x = context, y = diff_block, fill = condition))+
+  stat_summary(fun = "identity", geom = "col",position = "dodge")+
+  theme_bw()+
+  annotate("text",label = "FASTER IN BLOCK 1",x=2.25,y = 125,size = 8)+
+  annotate("text",label = "FASTER IN BLOCK 2",x=1.75,y = -300,size = 8)
+
+
+## Just looking at block 1
+ggplot(d %>% filter(block == 2), aes(x = context, y = cue_rt_mili, fill = condition))+
+  stat_summary(fun = "mean", geom = "col", position = "dodge")+
+  geom_text(stat = "summary",fun = "mean",vjust = 12, aes(label = round(after_stat(y),2)),
+            position = position_dodge(0.9))+
+  theme_bw()
+
+
+glmer_fit <- glmer(
+  cue_rt_mili ~ context * condition   + (1 | cue) + (1 | participant),
+  data = d,
+  family = inverse.gaussian("identity")
+)
+
+summary(glmer_fit)
+
+em_condition <- emmeans(glmer_fit,~context|condition)
+pairs(em_condition)
+em_context <- emmeans(glmer_fit,~condition|context)
+pairs(em_context)
+
+glmer_plot_main <-  d %>%
+  group_by(condition,context) %>%
+  get_summary_stats(cue_rt_mili, type = c('mean_se'))
+
+ggplot(glmer_plot_main, aes(x = condition, y = mean, fill = context))+
+  geom_col(position = "dodge")+
+  geom_errorbar(aes(ymin = mean - se, ymax = mean + se),
+                position = position_dodge(0.9),
+                width = 0.2)+
+  geom_text(stat = "identity", aes(label = after_stat(y)), vjust = 45,
+            position = position_dodge(0.9))
+
+glmer_plot_type <- word_assoc_filt %>%
+  group_by(condition,type,strength_strat,context) %>%
+  get_summary_stats(cue_rt_mili, type = c('mean_se'))
+
+ggplot(glmer_plot_type, aes(x = type, y = mean, fill = strength_strat))+
+  geom_col(position = "dodge")+
+  facet_grid(context~condition)
+
+
+ggplot(d %>%
+         select(context,
+                condition,
+                aoa,
+                Lg10WF,
+                Lg10CD,
+                nchar) %>%
+         pivot_longer(cols = c("aoa",starts_with("Lg"),"nchar"),
+                      names_to = "measure",
+                      values_to = "value"), aes(x = context, y = value))+
+  stat_summary(fun = "mean",geom = "bar")+
+  facet_grid(measure~condition, scales= "free_y")
+
+ez_long <- word_assoc_filt %>%
+  select(participant,
+         cue,
+         context,
+         condition,
+         aoa,
+         Lg10WF,
+         Lg10CD,
+         nchar) %>%
+  pivot_longer(cols = c("aoa",starts_with("Lg"),"nchar"),
+               names_to = "measure",
+               values_to = "value") %>%
+  drop_na()
