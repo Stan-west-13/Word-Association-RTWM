edf2015e56495977bc5df60304f4dd80e4e4a6f7
Stanley H West <swest19@lsu.edu>
2026-03-05 10:38:59 -0600

    first pass creativity-stereotypy

---
+++ 006-Stereotypy_Creative.R
@@ -0,0 +1,39 @@
+library(ggplot2)
+library(dplyr)
+library(readr)
+library(tidyr)
+library(lme4)
+library(rstatix)
+library(lmerTest)
+library(emmeans)
+source("R/Load_Helpers.R")
+
+z <- function(x){
+  return((x - mean(x,na.rm = T))/sd(x,na.rm = T))
+}
+
+
+## Load data 
+d <- load_most_recent_by_mtime("data/", pattern = "TTA2_meta_response_filtered-")
+
+## Stereotypy & Creative scores by context
+d_sc <- d %>%
+  group_by(context,cue) %>%
+  count(response, sort = TRUE,name = "resp_count") %>%
+  left_join(d, by = c("response","cue","context")) %>%
+  group_by(context,cue) %>%
+  mutate(mst_freq = ifelse(resp_count == max(resp_count), TRUE,FALSE)) %>%
+  group_by(context,cue,response) %>%
+  mutate(is_unique = ifelse(resp_count == 1,TRUE,FALSE)) %>%
+  group_by(participant) %>%
+  mutate(sum_stereo = sum(mst_freq),
+         sum_creative = sum(is_unique))
+
+
+ggplot(d_sc, aes(x = context, y = sum_stereo, fill = condition))+
+  stat_summary(fun = "mean", geom = "col", position = "dodge") +
+  labs(y = "avg_stereptypy")
+
+ggplot(d_sc, aes(x = context, y = sum_creative, fill = condition))+
+  stat_summary(fun = "mean", geom = "col", position = "dodge")+
+  labs(y = "avg_creativity")
